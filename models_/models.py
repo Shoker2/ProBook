eb20a6391aa7639b14b5779ed6bcf95fa0b895ac
Shoker2 <TheMallisteri@mail.ru>
2024-11-28 21:46:12 +0300

    новая версия БД

--- models_/models.py
+++ models_/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Integer, Column, String, TIMESTAMP,ARRAY, Boolean, CheckConstraint, Float, ForeignKey, UUID
+from sqlalchemy import MetaData, Table, Integer, Column, String, TIMESTAMP,ARRAY, Boolean, CheckConstraint, Float, ForeignKey, UUID, TEXT
 from datetime import datetime
 
 meta_data = MetaData()
@@ -15,8 +15,44 @@ user = Table(
     "user",
     meta_data, 
     Column("uuid", UUID, nullable = False, index=True, primary_key=True),
-    Column("is_superuser", Boolean, default=False, nullable=False),
-    Column("group_id", ForeignKey("group.id"), nullable = False)
+    Column("is_superuser", Boolean, server_default="false", nullable=False),
+    Column("group_id", ForeignKey("group.id"), nullable = True)
 )
 
 
+room = Table(
+    "room",
+    meta_data, 
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("capacity", Integer, nullable=False)
+)
+
+personal_reservation = Table(
+    "personal_reservation",
+    meta_data, 
+    Column("id", Integer, primary_key=True),
+    Column("room_id", ForeignKey("room.id"), nullable=False),
+    Column("user_uuid", ForeignKey("user.uuid"), nullable=False),
+    Column("info_for_moderator", TEXT, nullable=False),
+    Column("date", TIMESTAMP, nullable=False),
+    Column("moderated", Boolean, nullable=False, server_default="false")
+)
+
+event = Table(
+    "event",
+    meta_data, 
+    Column("id", Integer, primary_key=True),
+    Column("room_id", ForeignKey("room.id"), nullable=False),
+    Column("user_uuid", ForeignKey("user.uuid"), nullable=False),
+    Column("info_for_moderator", TEXT, nullable=False),
+
+    Column("title", String, nullable=False),
+    Column("description", TEXT, nullable=False),
+    Column("participants", ARRAY(UUID), nullable=False, server_default="{}"),
+    Column("img", String),
+    Column("repeat", String), # No (None) | Every: day, week, two week
+
+    Column("date", TIMESTAMP, nullable=False),
+    Column("moderated", Boolean, nullable=False, server_default="false")
+)
