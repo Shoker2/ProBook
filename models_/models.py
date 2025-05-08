a23d274c32fd1284701ddb51d1838161c84c7b75
Shoker2 <TheMallisteri@mail.ru>
2025-05-08 13:14:30 +0300

    Индексы в БД добавлены

--- models_/models.py
+++ models_/models.py
@@ -36,7 +36,7 @@ item = Table(
     meta_data,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
-    Column("room_id", ForeignKey("room.id"), nullable=True)
+    Column("room_id", ForeignKey("room.id"), nullable=True, index=True)
 )
 
 user = Table(
@@ -44,7 +44,7 @@ user = Table(
     meta_data,
     Column("uuid", UUID, nullable=False, index=True, primary_key=True),
     Column("is_superuser", Boolean, server_default="false", nullable=False),
-    Column("group_id", ForeignKey("group.id"), nullable=True)
+    Column("group_id", ForeignKey("group.id"), nullable=True, index=True)
 )
 
 
@@ -54,7 +54,7 @@ room = Table(
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
     Column("capacity", Integer, nullable=False),
-    Column("img", String),
+    Column("img", String, index=True),
     Column("description", TEXT, nullable=False, server_default="")
 )
 
@@ -62,14 +62,14 @@ personal_reservation = Table(
     "personal_reservation",
     meta_data,
     Column("id", Integer, primary_key=True),
-    Column("room_id", ForeignKey("room.id"), nullable=False),
-    Column("user_uuid", ForeignKey("user.uuid"), nullable=False),
+    Column("room_id", ForeignKey("room.id"), nullable=False, index=True),
+    Column("user_uuid", ForeignKey("user.uuid"), nullable=False, index=True),
     Column("info_for_moderator", TEXT, nullable=False),
     Column("needable_items", ARRAY(Integer),
            nullable=False, server_default="{}"),
-    Column("date_start", TIMESTAMP, nullable=False),
-    Column("date_end", TIMESTAMP, nullable=False),
-    Column("status", SMALLINT, nullable=False, server_default="0"),
+    Column("date_start", TIMESTAMP, nullable=False, index=True),
+    Column("date_end", TIMESTAMP, nullable=False, index=True),
+    Column("status", SMALLINT, nullable=False, server_default="0", index=True ),
     
     Column("cause_cancel", TEXT, nullable=False, server_default="")
 )
@@ -80,8 +80,8 @@ event = Table(
     Column("id", Integer, primary_key=True),
     Column("event_base_id", Integer, EVENT_BASE_ID_SEQ, nullable=False, server_default=EVENT_BASE_ID_SEQ.next_value()),
 
-    Column("room_id", ForeignKey("room.id"), nullable=False),
-    Column("user_uuid", ForeignKey("user.uuid"), nullable=False),
+    Column("room_id", ForeignKey("room.id"), nullable=False, index=True),
+    Column("user_uuid", ForeignKey("user.uuid"), nullable=False, index=True),
     Column("info_for_moderator", TEXT, nullable=False),
 
     Column("title", String, nullable=False),
@@ -89,12 +89,12 @@ event = Table(
     Column("participants", ARRAY(UUID), nullable=False, server_default="{}"),
     Column("needable_items", ARRAY(Integer),
            nullable=False, server_default="{}"),
-    Column("img", String),
-    Column("repeat", String),  # No (None) | Every: day, week, two week
+    Column("img", String, index=True),
+    Column("repeat", String, index=True),
 
-    Column("date_start", TIMESTAMP, nullable=False),
-    Column("date_end", TIMESTAMP, nullable=False),
-    Column("status", SMALLINT, nullable=False, server_default="0"), # 0 - Not moderated, 1 - approve, 2 - reject
+    Column("date_start", TIMESTAMP, nullable=False, index=True),
+    Column("date_end", TIMESTAMP, nullable=False, index=True),
+    Column("status", SMALLINT, nullable=False, server_default="0", index=True), # 0 - Not moderated, 1 - approve, 2 - reject
 
     Column("cause_cancel", TEXT, nullable=False, server_default="")
 )
@@ -103,9 +103,9 @@ schedule = Table(
     "schedule",
     meta_data,
     Column("id", Integer, primary_key=True),
-    Column("date", DATE, nullable=False),
+    Column("date", DATE, nullable=False, index=True),
     Column("schedule_time", ARRAY(String), nullable=False),
-    Column("room_id", ForeignKey("room.id"), nullable=False)
+    Column("room_id", ForeignKey("room.id"), nullable=False, index=True)
 )
 
 
@@ -113,8 +113,8 @@ action_history = Table(
     "action_history",
     meta_data,
     Column("id", Integer, primary_key=True),
-    Column("action", String, nullable=False),
-    Column("date", TIMESTAMP, nullable=False, default=datetime.utcnow),
+    Column("action", String, nullable=False, index=True),
+    Column("date", TIMESTAMP, nullable=False, default=datetime.utcnow, index=True),
     Column("subject_uuid", ForeignKey("user.uuid"), nullable=False),
     Column("object_table", String, nullable=False),
     Column("object_id", String, nullable=False),
@@ -124,5 +124,5 @@ action_history = Table(
 worker = Table(
     "worker",
     meta_data,
-    Column("user_uuid", ForeignKey("user.uuid"), nullable=False)
+    Column("user_uuid", ForeignKey("user.uuid"), nullable=False, primary_key=True)
 )
